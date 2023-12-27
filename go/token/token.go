3a001e11af9ad41427af94f24bd6945c2071cc3b
Richard Bieringa <richardbieringa@gmail.com>
2023-12-27 13:29:38 +0100

    feat: add token struct + constants

---
+++ go/token/token.go
@@ -0,0 +1,35 @@
+package token
+
+// Represents the type of the token, e.g. an INT or an IDENTIFIER
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string // The literal value of the token
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
