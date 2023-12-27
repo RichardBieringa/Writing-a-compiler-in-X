d18a9c9ef38eeb233c5cc4440a9f44863d8102d5
Richard Bieringa <richardbieringa@gmail.com>
2023-12-27 14:22:54 +0100

    feat: add more tests

--- go/lexer/lexer_test.go
+++ go/lexer/lexer_test.go
@@ -36,3 +36,73 @@ func TestNextToken(t *testing.T) {
 		}
 	}
 }
+
+func TestSourceCode(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+};
+let result = add(five, ten);
+`
+
+	tests := []struct {
+		Type    token.TokenType
+		Literal string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	lexer := New(input)
+
+	for i, expected := range tests {
+		actual := lexer.NextToken()
+
+		if actual.Type != expected.Type {
+			t.Fatalf("tests[%d] - incorrect token type: expected=%q, got=%q", i, expected.Type, actual.Type)
+		}
+
+		if actual.Literal != expected.Literal {
+			t.Fatalf("tests[%d] - incorrect token literal: expected=%q, got=%q", i, expected.Type, actual.Type)
+		}
+	}
+}
