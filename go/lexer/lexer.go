02ec20f512871f365c70869f7f3684a3070804e4
Richard Bieringa <richardbieringa@gmail.com>
2023-12-28 15:54:52 +0100

    reimplement identifiers ASCII only

--- go/lexer/lexer.go
+++ go/lexer/lexer.go
@@ -55,9 +55,45 @@ func (l *Lexer) NextToken() token.Token {
 	case '}':
 		t.Type = token.RBRACE
 	default:
-		t.Type = token.ILLEGAL
+		if isLetter(l.currentChar) {
+			t.Literal = l.readIdentifier()
+			t.Type = getIdentifier(t.Literal)
+		} else {
+			t.Type = token.ILLEGAL
+		}
 	}
 
 	l.readChar()
 	return t
+}
+
+func isLetter(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_'
+}
+
+func isNumber(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func getIdentifier(identifier string) token.TokenType {
+	keywords := map[string]token.TokenType{
+		"let": token.LET,
+	}
+
+	tokenType, ok := keywords[identifier]
+	if ok {
+		return tokenType
+	}
+
+	return token.IDENT
+}
+
+func (l *Lexer) readIdentifier() string {
+	left := l.position
+
+	for isLetter(l.currentChar) {
+		l.readChar()
+	}
+
+	return l.input[left:l.position]
 }
