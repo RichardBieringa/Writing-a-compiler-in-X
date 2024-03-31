98272d06d9c6109029e7b95ea881818f34dd63d4
Richard Bieringa <RichardBieringa@gmail.com>
2024-03-31 18:35:48 +0200

    fix: LT GT operator precedence

--- go/parser/operator_precedence.go
+++ go/parser/operator_precedence.go
@@ -7,18 +7,19 @@ import "monkey/token"
 const (
 	_ int = iota
 	LOWEST
-	EQUALS  // == LESSGREATER // > or <
-	SUM     //+
-	PRODUCT //*
-	PREFIX  //-Xor!X
-	CALL    // myFunction(X)
+	EQUALS      // =
+	LESSGREATER // < or >
+	SUM         // +
+	PRODUCT     //*
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
 )
 
 var precedenceMap = map[token.TokenType]int{
 	token.EQ:       EQUALS,
 	token.NOT_EQ:   EQUALS,
-	token.LT:       EQUALS,
-	token.GT:       EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
 	token.PLUS:     SUM,
 	token.MINUS:    SUM,
 	token.SLASH:    PRODUCT,
